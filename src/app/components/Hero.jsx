9a31b0b51d2d5a8a863b2150e33a74fcf03f7d08
IfeoluwaGIT <idurobello@gmail.com>
2024-09-15 23:57:08 +0100

    try

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -4,14 +4,14 @@ const Hero = () => {
   return (
     <section className="flex flex-col items-center justify-center px-6 py-12 md:px-12 lg:px-20 xl:px-[20%]">
       <div className="text-center">
-        <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold text-color mt-6 md:mt-8 lg:mt-10">
+        <h1 className="text-[30px] md:text-4xl lg:text-4xl font-bold text-color mt-6 md:mt-8 lg:mt-10">
           Global Workforce Solutions for a Competitive Edge
         </h1>
         <p className="text-color mb-4 md:mb-6">
           Simplifying global recruitment, staff and payroll management while offering comprehensive HR solutions
         </p>
         <Link href="/about">
-        <button className="px-4 py-2 md:px-6 md:py-3 bg-text-color text-white rounded hover:bg-button-hover">
+        <button className="px-[16px] py-[8px] md:px-6 md:py-3 bg-text-color text-white rounded hover:bg-button-hover">
           Learn More
         </button>
         </Link>
